f3e3fe8d5c4a5d606d9a12b511a2d3ddba540943
Igor Zmitrovich <igor@zmitrovi.ch>
2022-08-03 16:39:34 +0200

    Use service statuses for enabling the button

--- Assets/NPCEngine/Demo/BasicDemo/Scripts/ButtonDisableEnable.cs
+++ Assets/NPCEngine/Demo/BasicDemo/Scripts/ButtonDisableEnable.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Linq;
+using NPCEngine.API;
 using NPCEngine.Components;
 using NPCEngine.Utility;
 using UnityEngine;
@@ -7,12 +8,12 @@ using UnityEngine.UI;
 
 public class ButtonDisableEnable : MonoBehaviour
 {
-    private Button _button;
+    private Button button;
     public string serviceName;
     void Start()
     {
-        _button = gameObject.GetComponent<Button>();
-        _button.interactable = false;
+        button = gameObject.GetComponent<Button>();
+        button.interactable = false;
         
         CoroutineUtility.StartCoroutine(EnableButtonWhenServiceRunning(), 
             this, "EnableButtonWhenServiceRunning");
@@ -22,17 +23,21 @@ public class ButtonDisableEnable : MonoBehaviour
     {
         while (true)
         {
-            
-            var services = NPCEngineManager.Instance.Services;
-            if (services != null && services.Count(serviceMetadata => serviceMetadata.service == serviceName) > 0)
+            var manager = NPCEngineManager.Instance;
+            var services = manager.Services;
+            if (services != null && services.Any())
             {
-                _button.interactable = true;
-                yield return null;
+                var statuses = manager.ServiceStatuses;
+                for (int i = 0; i < statuses.Count; i++)
+                {
+                    if (services[i].service == serviceName && statuses[i] == ServiceStatus.RUNNING)
+                    {
+                        button.interactable = true;
+                        yield return null;
+                    }
+                }
             }
-            else
-            {
-                yield return CoroutineUtility.WaitForSeconds(1f);
-            }
+            yield return CoroutineUtility.WaitForSeconds(1f);
         }
     }
 }
